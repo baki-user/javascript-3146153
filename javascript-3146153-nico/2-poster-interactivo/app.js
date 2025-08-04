6505cd8107e5f0e02e0df2754d63a0c0b434e281
Nicolás <nicolmaderamc123@gmail.com>
2025-08-04 16:08:28 -0500

    Add files via upload

---
+++ javascript-3146153-nico/2-poster-interactivo/app.js
@@ -0,0 +1,14 @@
+
+const monedas = document.querySelectorAll(".coin")
+const corazon = document.querySelectorAll(".corazon")
+const contadorMonedas = document.querySelector("#contador-monedas")
+const contadorCorazones = document.querySelector("#contador-corazones")
+
+console.log(monedas,corazon,contadorMonedas,contadorCorazones)
+
+monedas.forEach(function(moneda){
+    moneda.addEventListener( 'click', function() {
+        moneda.classList.add('saltar')
+    })
+  
+})
