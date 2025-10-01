2c922d7be735cc60d515d60b03836de600306317
Nicolás <nicolmaderamc123@gmail.com>
2025-10-01 14:31:21 -0500

    Add files via upload

---
+++ 6_Base-datos-comic/bd.js
@@ -0,0 +1,36 @@
+const comic = {
+    "nombreComic": "El Calentamiento Global y la Industralizacion",
+    "categoria" : "Distopico",
+    "cantidadCap" : 3,
+    "descripcion" : "Es una narrativa distopica centrada en el calentamiento global y la industrializacion, mostrada a traves de dos niños afectados de diferentes maneras por esto.",
+    "personajes" : ["Ossian", "Asher", "Sora"],
+    "bgBanner" : "assets/princi.png",
+    "bgFondo" : "assets/fondo.gif",
+    "capitulos" :[
+        {
+            id: 1,
+            nombreCap: "Ep 1: Semilla del abandono",
+            imgPortada: "assets/ep1.jpeg",
+            video: "source/TeaserECYDA.mp4",
+            descripcion: "Asher, un niño abandonado y convertido en una planta, lidera la venganza de la naturaleza contra la humanidad. Su destino se enfrenta al de Ossian, el último humano sobreviviente.",
+            personajes: ["Ossian", "Asher", "Sora"]
+        },
+        {
+            id: 2,
+            nombreCap: "Ep 2: El eco de la furia",
+            imgPortada: "assets/ep2.jpeg",
+            video: "source/TeaserECYDA.mp4",
+            descripcion: "En un mundo devastado por plantas descontroladas, Ossian pierde a su madre y hermana en un ataque misterioso. Solo en una ciudad vacía, jura vengarse y se refugia en un búnker, decidido a descubrir la verdad.",
+            personajes: ["Ossian", "Asher"]
+        },
+        {
+            id: 3,
+            nombreCap: "Ep 3: El ocaso de la humanidad",
+            imgPortada: "assets/ep3.jpeg",
+            video: "source/TeaserECYDA.mp4",
+            descripcion: "Ossian rompe su aislamiento y se enfrenta a Asher, un ser mita que confiesa un oscuo secreto. Entre odio y reflexión descubren el Proyecto Eden. Se establece una frágil tregua, pero con el tiempo Asher se convierte en algo inesperado y, ¿Ossian sobrevivirá? ",
+            personajes: ["Ossian", "Asher"]
+        }
+    ]
+
+}
